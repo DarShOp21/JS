5c33a9297b05166013033ed16b7f0314ac3b75f4
darshan <darshanayare43@gmail.com>
2024-06-05 14:16:20 +0530

    Update every.js with new changes

--- PART8/LEC/every.js
+++ PART8/LEC/every.js
@@ -1,4 +1,9 @@
  //it  gives only two values either true or false
  //It gives true when all the elements in an array satifies the conditionnit satisfies
 
- 
+  let arr = [1 , 2, 3, 4, 5];
+
+  let isAllEven = arr.every((el) => (el % 2 == 0));
+  let isAllGreaterThan0 = arr.every((el) => (el > 0));
+  console.log(`all elements of arr are even : ${isAllEven}`);
+  console.log(`all elements of arr are greater than 0 : ${isAllGreaterThan0}`);
